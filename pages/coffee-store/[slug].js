fe4a4dadc4c8ffc7d5f32f264c8f7268a14ec08b
jakareya76 <jakareya1306@gmail.com>
2023-04-18 15:47:06 +0600

    design individual store page

--- pages/coffee-store/[slug].js
+++ pages/coffee-store/[slug].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 
@@ -16,8 +18,16 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = () => {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        slug: coffeeStore.id.toString(),
+      },
+    };
+  });
+
   return {
-    paths: [{ params: { slug: "0" } }, { params: { slug: "1" } }],
+    paths,
     fallback: true,
   };
 };
@@ -29,11 +39,78 @@ const slug = ({ coffeeStores }) => {
     return <h1 className="text-white text-xl">Loading...</h1>;
   }
 
+  const handleUpVote = () => {
+    console.log("Up Vote");
+  };
+
   return (
-    <div className="text-white text-xl">
-      <p>{coffeeStores.name}</p>
-      <p>{coffeeStores.address}</p>
-    </div>
+    <>
+      <Head>
+        <title>{coffeeStores.name}</title>
+      </Head>
+      <main className="container mx-auto flex flex-col justify-center py-20 md:py-44">
+        <div className="w-full flex">
+          <button className="bg-zinc-800 px-6 py-4 text-zinc-400 font-semibold rounded-md mx-5">
+            Back To Home
+          </button>
+        </div>
+        <div className="bg-zinc-800 my-10 py-12 px-8 mx-5">
+          <h1 className="text-4xl text-center underline mb-8 text-zinc-400">
+            {coffeeStores.name}
+          </h1>
+          <div className="flex flex-col items-center justify-center gap-10 py-10 md:flex-row">
+            <Image
+              src={coffeeStores.imgUrl}
+              alt="img"
+              width={420}
+              height={260}
+              className="max-h-[360px] w-full object-cover rounded md:w-[500px]"
+            />
+            <div className="w-full md:w-1/2">
+              <div className="flex space-x-2 py-2">
+                <Image
+                  src="/svg/location.svg"
+                  width={24}
+                  height={24}
+                  className="fill-zinc-400"
+                />
+                <p className="text-zinc-400 text-md font-semibold md:text-xl">
+                  {coffeeStores.address}
+                </p>
+              </div>
+              <div className="flex space-x-2 py-2">
+                <Image
+                  src="/svg/near.svg"
+                  width={24}
+                  height={24}
+                  className="fill-zinc-400"
+                />
+                <p className="text-zinc-400 text-md font-semibold md:text-xl">
+                  {coffeeStores.neighbourhood}
+                </p>
+              </div>
+              <div className="flex space-x-2 py-2">
+                <Image
+                  src="/svg/star.svg"
+                  width={24}
+                  height={24}
+                  className="fill-zinc-400"
+                />
+                <p className="text-zinc-400 text-md font-semibold md:text-xl">
+                  1
+                </p>
+              </div>
+              <button
+                onClick={handleUpVote}
+                className="px-12 py-4 bg-zinc-900 text-zinc-400 text-xl font-semibold rounded-md mt-10 duration-200 hover:bg-zinc-950"
+              >
+                Vote Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Hero from "../components/Hero";
 import Card from "@/components/Card";
 
@@ -15,32 +17,37 @@ export default function Home({ coffeeStores }) {
   const handleOnBannerBtnClick = () => {};
 
   return (
-    <main className="container mx-auto">
-      <Hero
-        buttonText="View stores nearby"
-        handleOnClick={handleOnBannerBtnClick}
-      />
-      <div className="flex items-center flex-col py-10">
-        {coffeeStores.length > 0 ? (
-          <div className="w-full flex items-center justify-center">
-            <h2 className="text-4xl font-mono font-bold text-zinc-300 mb-10 underline">
-              Toronto Stores
-            </h2>
+    <>
+      <Head>
+        <title>COFFEE CONNOISSEUR</title>
+      </Head>
+      <main className="container mx-auto">
+        <Hero
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className="flex items-center flex-col py-10">
+          {coffeeStores.length > 0 ? (
+            <div className="w-full flex items-center justify-center">
+              <h2 className="text-4xl font-mono font-bold text-zinc-300 mb-10 underline">
+                Toronto Stores
+              </h2>
+            </div>
+          ) : null}
+          <div className="grid grid-cols-1 place-items-center gap-10 md:grid-cols-2 lg:grid-cols-3">
+            {coffeeStores.map((coffeeStore) => {
+              return (
+                <Card
+                  name={coffeeStore.name}
+                  img={coffeeStore.imgUrl}
+                  url={`/coffee-store/${coffeeStore.id}`}
+                  key={coffeeStore.id}
+                />
+              );
+            })}
           </div>
-        ) : null}
-        <div className="grid grid-cols-1 place-items-center gap-10 md:grid-cols-2 lg:grid-cols-3">
-          {coffeeStores.map((coffeeStore) => {
-            return (
-              <Card
-                name={coffeeStore.name}
-                img={coffeeStore.imgUrl}
-                url={`/coffee-store/${coffeeStore.id}`}
-                key={coffeeStore.id}
-              />
-            );
-          })}
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
